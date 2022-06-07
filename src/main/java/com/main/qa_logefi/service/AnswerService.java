298fa463d77b05b87e9aa8bdce793c343aada2c1
CHENZhoujing <zhoujingchen1@gmail.com>
2022-06-07 23:13:41 +0200

    删去部分不必要注释

--- src/main/java/com/main/qa_logefi/service/AnswerService.java
+++ src/main/java/com/main/qa_logefi/service/AnswerService.java
@@ -9,30 +9,11 @@ import java.util.List;
  */
 public interface AnswerService {
 
-    /**
-     * 根据用户编号查询回答
-     * @param user_id
-     * @return
-     */
     List<Answer> findAnswerByUserId(Integer user_id);
 
-    /**
-     * 新增回答
-     * 回答者用户编号、回答所对应的问题编号为必填项
-     * @param answer
-     */
     void saveAnswer(Answer answer);
 
-    /**
-     * 更新回答
-     * 回答编号、回答者用户编号、回答所对应的问题编号为必填项
-     * @param answer
-     */
     void updateAnswer(Answer answer);
 
-    /**
-     * 根据ID删除回答
-     * @param answer_id
-     */
     void deleteAnswer(Integer answer_id);
 }
